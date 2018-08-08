9cebaa2e92df867376ac3c8af58f5f1acb7eb4b7
ElonaKadukov <elonak24@yahoo.com>
2018-08-08 16:10:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
 const currentUser= 'Grace Hopper';
 const welcomeMessage= 'Welcome to Klossybook, '+currentUser+'!'
-const excitedWelcomeMessage= welcomeMessage.toUppercase();
+const excitedWelcomeMessage= welcomeMessage.toUpperCase();
 
